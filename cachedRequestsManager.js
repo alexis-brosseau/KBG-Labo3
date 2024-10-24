1289f426e3e1d8673fe5ce23739e6af6eca6da01
Alexis Brosseau <alexisbrosseau@outlook.com>
2024-10-23 21:03:26 -0400

    commit

---
+++ cachedRequestsManager.js
@@ -0,0 +1,79 @@
+import * as utilities from "./utilities.js";
+import * as serverVariables from "./serverVariables.js";
+
+const CACHES_EXPIRATION_TIME = serverVariables.get("main.requests.CacheExpirationTime");
+
+global.requestsCaches = [];
+global.cachedRequestsCleanerStarted = false;
+
+export default class CachedRequestsManager {
+
+
+    static startCachedRequestsCleaner() {
+        setInterval(CachedRequestsManager.flushExpired, CACHES_EXPIRATION_TIME * 1000);
+        console.log(BgWhite + FgBlue, "[Periodic requests caches cleaning process started...]");
+    }
+
+    static add(url, content, ETag="") {
+        if (!cachedRequestsCleanerStarted) {
+            cachedRequestsCleanerStarted = true;
+            CachedRequestsManager.startCachedRequestsCleaner();
+        }
+        if (url != "") {
+            CachedRequestsManager.clear(url);
+            requestsCaches.push({
+                url,
+                content,
+                ETag,
+                Expire_Time: utilities.nowInSeconds() + CACHES_EXPIRATION_TIME
+            });
+            console.log(BgWhite + FgBlue, `[Data of ${url} request has been cached]`);
+        }
+    }
+    
+    static find(url) {
+        try {
+            if (url != "") {
+                for (let cache of requestsCaches) {
+                    if (cache.url == url) {
+                        cache.Expire_Time = utilities.nowInSeconds() + CACHES_EXPIRATION_TIME;
+                        console.log(BgWhite + FgBlue, `[${cache.url} data retrieved from cache]`);
+                        return cache;
+                    }
+                }
+            }
+        } catch (error) {
+            console.log(BgWhite + FgRed, "[request cache error!]", error);
+        }
+        return null;
+    }
+    static clear(url) {
+        if (url != "") {
+            let indexToDelete = [];
+            let index = 0;
+            for (let cache of requestsCaches) {
+                if (cache.url == url) indexToDelete.push(index);
+                index++;
+            }
+            utilities.deleteByIndex(requestsCaches, indexToDelete);
+        }
+    }
+    static flushExpired() {
+        let now = utilities.nowInSeconds();
+        for (let cache of requestsCaches) {
+            if (cache.Expire_Time <= now) {
+                console.log(BgWhite + FgBlue, "Cached request data of " + cache.url + " expired");
+            }
+        }
+        requestsCaches = requestsCaches.filter( cache => cache.Expire_Time > now);
+    }
+    static get(HttpContext) {
+        let url = HttpContext.req.url;
+        let cache = CachedRequestsManager.find(url);
+        if (cache) {
+            HttpContext.response.JSON(cache.content, cache.ETag, true);
+            return true;
+        }
+        return false;
+    }
+}
--- response.js
+++ response.js
@@ -5,6 +5,7 @@
 // Lionel-Groulx College
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 import { log } from "./log.js";
+import CachedRequestsManager from "./cachedRequestsManager.js"; 
 
 export default class Response {
     constructor(HttpContext) {
@@ -40,11 +41,21 @@ export default class Response {
         this.res.writeHead(204, { 'ETag': ETag });
         this.end();
     }
-    JSON(obj, ETag = "") {                         // ok status with content
+    JSON(obj, ETag = "", fromCache = false) {                         // ok status with content
         if (ETag != "")
             this.res.writeHead(200, { 'content-type': 'application/json', 'ETag': ETag });
         else
             this.res.writeHead(200, { 'content-type': 'application/json' });
+
+        /* Modifier la méthode HttpContext.response.JSON(jsonObj, ETag="", fromCache = false ). Si le paramètre fromCache est
+        faux ajouter dans la cache si la requête est de type API et que le id est non défini */
+        if (!fromCache) {
+            if (this.HttpContext.path.isAPI && this.HttpContext.path.id == undefined) {
+                CachedRequestsManager.add(this.HttpContext.req.url, obj, ETag);
+            }
+        };
+            
+
         if (obj != null) {
             let content = JSON.stringify(obj);
             console.log(FgCyan+Bright, "Response payload -->", content.toString().substring(0, 75) + "...");
